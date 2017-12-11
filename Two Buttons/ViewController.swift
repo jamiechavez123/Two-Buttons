651410353bb488010b83e06862721bd3f46b5e66
jamie <jchavez@lincolnschool.org>
2017-12-11 11:29:33 -0500

    Initial Commit

---
+++ Two Buttons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Two Buttons
+//
+//  Created by Student on 12/11/17.
+//  Copyright © 2017 Jamie Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
